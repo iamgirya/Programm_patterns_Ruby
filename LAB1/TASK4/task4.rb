dbdeffc228321979d3f8222eaf31044788b57aee
iamgirya <84345515+iamgirya@users.noreply.github.com>
2023-02-11 09:53:13 +0300

    Task 4_2

--- LAB1/TASK4/task4.rb
+++ LAB1/TASK4/task4.rb
@@ -1,19 +1,14 @@
+require 'set'
 # 1.2 Дан целочисленный массив. Необходимо найти индекс минимального элемента.
 def indOfMinEl(arr)
   return arr.index(arr.min)
 end
-
 # 1.14 Дан целочисленный массив и интервал a..b. Необходимо найти количество элементов в этом интервале.
+def countOfElInInterval(arr, interval)
+  return (arr.select {|element| (element >= interval[0] && element <= interval[1])  }).length
+end
 
-# 1.26 Дан целочисленный массив. Необходимо найти количество элементов между первым и последним минимальным.
 
-# 1.38 Дан целочисленный массив и отрезок a..b. Необходимо найти количество элементов, 
-# значение которых принадлежит этому отрезку.
-
-# 1.50. Для двух введенных списков L1 и L2 построить новый список, состоящий из элементов,
-# встречающихся только в одном из этих списков и не повторяющихся в них.
-
-# arr = [4,6,1,7,3,5,6,1,2,4,3]
 puts "Введите массив для обработки"
 arr = gets.chomp.split(' ').map(&:to_i)
 puts "Введённый массив: #{arr.inspect}"
@@ -32,16 +27,21 @@ if (n == 2 || n == 4) then
   ab = gets.chomp.split(' ').map(&:to_i)
   puts "Введённый интервал: #{ab}"
 end
+if (n == 5) then
+  puts "Введите второй список"
+  arr2 = gets.chomp.split(' ').map(&:to_i)
+  puts "Введённый интервал: #{arr2}"
+end
 
 case n
 when 1
   puts "Результат: #{indOfMinEl(arr)}"
 when 2
-  puts "Результат: #{indOfMinEl(arr, ab)}"
+  puts "Результат: #{countOfElInInterval(arr, ab)}"
 when 3
-  puts "Результат: #{indOfMinEl(arr)}"
+  puts "Результат: #{countOfElBetweenFirstMinAndSecondMin(arr)}"
 when 4
-  puts "Результат: #{indOfMinEl(arr, ab)}"
+  puts "Результат: #{countOfElInOtrezok(arr, ab)}"
 when 5
-  puts "Результат: #{indOfMinEl(arr)}"
+  puts "Результат: #{delta(arr, arr2)}"
 end
