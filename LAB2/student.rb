dfd95f2991abfb5e420f82d2876527f1eb60440f
iamgirya <84345515+iamgirya@users.noreply.github.com>
2023-02-26 22:21:51 +0300

    lab2/task1/8

--- LAB2/student.rb
+++ LAB2/student.rb
@@ -2,13 +2,27 @@
 
 class Student
     # стандартные геттеры и сеттеры для класса
-    attr_accessor :id, :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
+    attr_accessor :id
+    attr_reader :phone, :git, :telegram, :email, :last_name, :first_name, :paternal_name
   
     #валидатор номера телефона
     def self.valid_phone?(phone)
       phone.match(/^\+?[7,8] ?\(?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$/)
     end
- 
+  
+    #валидатор имени
+    def self.valid_name?(name)
+      name.match(/^[А-Я][а-я]+$/)
+    end
+  
+    #валидатор профиля
+    def self.valid_account?(account)
+      account.match(/^@[A-Za-z0-9\-_]+$/)
+    end
+  
+    def self.valid_email?(email)
+      email.match(/^[A-Za-z0-9\-_]+@[A-Za-z]+\.([A-Za-z]+\.)*[A-Za-z]+$/)
+    end
     # конструктор
     def initialize(last_name, first_name, paternal_name, options = {})
       self.last_name = last_name
@@ -26,6 +40,42 @@ class Student
       raise ArgumentError, "Incorrect value: phone=#{phone}!" if !phone.nil? && !Student.valid_phone?(phone)
   
       @phone = phone
+    end
+  
+    def first_name=(first_name)
+      raise ArgumentError, "Incorrect value: first_name=#{first_name}!" if !first_name.nil? && !Student.valid_name?(first_name)
+  
+      @first_name = first_name
+    end
+  
+    def last_name=(last_name)
+      raise ArgumentError, "Incorrect value: last_name=#{last_name}!" if !last_name.nil? && !Student.valid_name?(last_name)
+  
+      @last_name = last_name
+    end
+  
+    def paternal_name=(paternal_name)
+      raise ArgumentError, "Incorrect value: paternal_name=#{paternal_name}!" if !paternal_name.nil? && !Student.valid_name?(paternal_name)
+  
+      @paternal_name = paternal_name
+    end
+  
+    def git=(git)
+      raise ArgumentError, "Incorrect value: git=#{git}!" if !git.nil? && !Student.valid_account?(git)
+  
+      @git = git
+    end
+  
+    def telegram=(telegram)
+      raise ArgumentError, "Incorrect value: telegram=#{telegram}!" if !telegram.nil? && !Student.valid_account?(telegram)
+  
+      @telegram = telegram
+    end
+  
+    def email=(email)
+      raise ArgumentError, "Incorrect value: email=#{email}!" if !email.nil? && !Student.valid_email?(email)
+  
+      @email = email
     end
   
     def to_s
