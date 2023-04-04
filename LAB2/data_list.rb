c31dcd70e4b825476cb05be5da0651573fb7e00a
iamgirya <iamgirya@yandex.ru>
2023-04-04 15:14:50 +0300

    lab2/task3/4

--- LAB2/data_list.rb
+++ LAB2/data_list.rb
@@ -1,7 +1,25 @@
 class DataList 
     private_attr_accessor :list
+    private_attr_accessor :selected_objects
 
     def initialize(list:)
         self.list = list
+    end
+
+    def select(index)
+        selected_objects << index
+    end
+
+    def get_selected_objects
+        (0..<list.size)
+            .filter { |x| selected_objects.include? x }
+    end
+
+    def get_names
+        raise "Метод не реализован"
+    end
+
+    def get_data
+        raise "Метод не реализован"
     end
 end
