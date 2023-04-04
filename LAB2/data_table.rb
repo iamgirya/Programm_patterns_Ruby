bc95668bb570384c7e3c42f6816f1b7b2a8f2d75
iamgirya <iamgirya@yandex.ru>
2023-04-04 15:07:07 +0300

    lab2/task3/2

--- LAB2/data_table.rb
+++ LAB2/data_table.rb
@@ -1,7 +1,19 @@
-class DataTable {
+class DataTable 
     private_attr_accessor :data
 
     def initialize(data:)
         self.data = data
     end
-}
+
+    def element(row:, column:)
+        data[row][column]
+    end
+
+    def columnsCount 
+        data[0].size
+    end
+
+    def rowsCount
+        data.size
+    end
+end
