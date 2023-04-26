c4302ab09f390063db622d8c987acb00d4e74a2b
iamgirya <iamgirya@yandex.ru>
2023-04-26 09:33:48 +0300

    lab3.1.5

--- LAB2/main.rb
+++ LAB2/main.rb
@@ -5,4 +5,5 @@ db = StudentsDB.new()
 client = Mysql2::Client.new(:host => "localhost", :username => "root")
 results = client.query("USE my_db")
 results = client.query("SELECT * FROM student")
+results = client.query("SELECT * FROM student WHERE id = 3")
 puts results.map { |x| x.to_s }
